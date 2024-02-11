a9cca1a0352760aa076fbdbe3acfaa0eca6dcafb
sikwan <yysskk99999@naver.com>
2024-02-11 17:12:30 +0900

    LaunchStory board

---
+++ C-Cermmerce/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  C-Cermmerce
+//
+//  Created by 양시관 on 2/11/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+    
+}

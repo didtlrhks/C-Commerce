a01f0ba4eee29f87fb217fa7cfddf45904fd114e
sikwan <yysskk99999@naver.com>
2024-02-11 17:27:19 +0900

    launchboard animation

--- C-Cermmerce/SplashViewController.swift
+++ C-Cermmerce/SplashViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    @IBOutlet var appIconCenterYConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var appIconCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var appIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .purple
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        appIconCenterXConstraint.constant = -(view.frame.width / 2) - (appIcon.frame.width / 2)
+        appIconCenterYConstraint.constant = -(view.frame.height / 2) - (appIcon.frame.height / 2)
+        
+        UIView.animate(withDuration: 2) {//후인 클로저 생략한는거 사용ㅎㅎㅎ
+            [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+        
     }
     
 }

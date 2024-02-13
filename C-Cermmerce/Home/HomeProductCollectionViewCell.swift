6ddd99677ac0d8ebeef932949fb5d76f0a62c797
sikwan <yysskk99999@naver.com>
2024-02-13 18:10:09 +0900

    PruductHoemCellView

---
+++ C-Cermmerce/Home/HomeProductCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  HomeProductCollectionViewCell.swift
+//  C-Cermmerce
+//
+//  Created by 양시관 on 2/13/24.
+//
+
+import UIKit
+
+
+struct HomeProductCollectionViewCellViewModel {
+    let imageUrlString : String
+    let title : String
+    let reasonDiscountString: String
+    let originalPrice : String
+    let discountPrice : String
+}
+
+
+class HomeProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var productItemImageView: UIImageView!
+    
+    @IBOutlet var productTitleLabel: UILabel!
+    @IBOutlet var productReasonDiscountLabel: UILabel!
+    @IBOutlet var originalPriceLabel: UILabel!
+    
+    @IBOutlet var discountPriceLabel: UILabel!
+    
+    
+    
+    func setViewModel(_ viewModel: HomeProductCollectionViewCellViewModel) {
+        productTitleLabel.text = viewModel.title
+        productReasonDiscountLabel.text = viewModel.reasonDiscountString
+        originalPriceLabel.attributedText = NSAttributedString(string: viewModel.originalPrice, attributes: [NSAttributedString.Key.strikethroughStyle:NSUnderlineStyle.single.rawValue]) 
+        discountPriceLabel.text = viewModel.discountPrice
+    }
+}

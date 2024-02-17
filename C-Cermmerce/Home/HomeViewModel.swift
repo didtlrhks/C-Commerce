ec983753431515143f851f2d114d66f421447828
sikwan <yysskk99999@naver.com>
2024-02-17 18:22:59 +0900

    source code refactoring with pattern MVI

--- C-Cermmerce/Home/HomeViewModel.swift
+++ C-Cermmerce/Home/HomeViewModel.swift
@@ -11,6 +11,8 @@ import Combine
 class HomeViewModel {
     enum Action {
         case loadData
+        case getDataSuccess(HomeResponse)
+        case getDataFailure(Error)
     }
     final class State{
         struct CollectionViewModels{
@@ -27,29 +29,40 @@ class HomeViewModel {
         switch action {
         case .loadData:
             loadData()
+        case let .getDataSuccess(response):
+            transformResponses(response)
+        case let .getDataFailure(error):
+            print("nework error : \(error)")
         }
     }
     func loadData() {
         loadDataTask = Task{
             do {
                 let response = try await NetworkService.shared.getHoemData()
-                Task{
-                    await transformBanner(response)
-                }
-                Task{
-                    await transformHorizontalProduct(response)
-                }
-                Task{
-                    await transformVerticalProduct(response)
-                }
+                process(action: .getDataSuccess(response))
+               
+              
             }
             catch {
-                print("nework error : \(error)")
+                process(action: .getDataFailure(error))
+               
             }
         }
     }
     deinit{
         loadDataTask?.cancel()
+    }
+    
+    private func transformResponses(_ response: HomeResponse){
+        Task{
+            await transformBanner(response)
+        }
+        Task{
+            await transformHorizontalProduct(response)
+        }
+        Task{
+            await transformVerticalProduct(response)
+        }
     }
     
     @MainActor
@@ -64,23 +77,23 @@ class HomeViewModel {
     
     @MainActor
     private func transformHorizontalProduct(_ response: HomeResponse) async {
-        state.collectionViewModels.horizontalProductViewModels = response.horizontalProducts.map{
-            HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
-                                                   title: $0.title,
-                                                   reasonDiscountString: $0.discount,
-                                                   originalPrice: "\($0.originalPrice)",
-                                                   discountPrice: "\($0.discountPrice)")
-        }
+        state.collectionViewModels.horizontalProductViewModels = productToHomeProductCollectionViewCellViewModel(response.horizontalProducts)
     }
     
     @MainActor
     private func transformVerticalProduct(_ response:HomeResponse) async {
-        state.collectionViewModels.verticalProductViewModels = response.verticalProducts.map{
-            HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
-                                                   title: $0.title,
-                                                   reasonDiscountString: $0.discount,
-                                                   originalPrice: "\($0.originalPrice)",
-                                                   discountPrice: "\($0.discountPrice)")
+        state.collectionViewModels.verticalProductViewModels = productToHomeProductCollectionViewCellViewModel(response.verticalProducts)
+    }
+    
+    private func productToHomeProductCollectionViewCellViewModel(_ product: [Product]) -> [HomeProductCollectionViewCellViewModel] {
+       
+            return product.map{
+                HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
+                                                                      title: $0.title,
+                                                                      reasonDiscountString: $0.discount,
+                                                                      originalPrice: "\($0.originalPrice)원",
+                                                                      discountPrice: "\($0.discountPrice)원")
+            }
         }
     }
-}
+

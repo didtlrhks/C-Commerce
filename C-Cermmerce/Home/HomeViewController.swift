50b6c45abec101e8704a4a3d5cdcfc2ff44009bc
sikwan <yysskk99999@naver.com>
2024-02-17 21:23:00 +0900

    homeviewcontroller

--- C-Cermmerce/Home/HomeViewController.swift
+++ C-Cermmerce/Home/HomeViewController.swift
@@ -23,30 +23,30 @@ class HomeViewController:UIViewController {
     private var compositianlLayout : UICollectionViewCompositionalLayout = setCompositinalLayout()
         
         //UICollectionViewCompositionalLayout를 사용하여 각 섹션의 레이아웃을 동적으로 생성
-    private var viewModel : HomeViewModel = HomeViewModel()
-    private var cancellables : Set<AnyCancellable> = []
+    private var viewModel : HomeViewModel = HomeViewModel() // 홈화면의 데이터 처리와 상태관리를 담당하기위해서 사용함
+    private var cancellables : Set<AnyCancellable> = [] // 콤바인을 사용해서 뷰모델의 상태변화를 구독하는 cancellables 을 만들어줌
 
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad() { // 뷰 컨트롤러의 뷰가 로드될때 호출되는녀석
         super.viewDidLoad()
       
-        bindingViewModel()
+        bindingViewModel()//뷰모델 바인딩
        
-        setDataSource()
+        setDataSource()// 데이터 소스설정하는 함수사용
         
       
         
-        collectionView.collectionViewLayout = compositianlLayout
+        collectionView.collectionViewLayout = compositianlLayout//컬렉션 뷰의 레이아웃설정
 
-        viewModel.process(action: .loadData)
+        viewModel.process(action: .loadData)//데이터 로드작업
     }
     
-    private static func setCompositinalLayout() -> UICollectionViewCompositionalLayout {
+    private static func setCompositinalLayout() -> UICollectionViewCompositionalLayout { // 각세션별 레이아웃을 설정하는 함수
         UICollectionViewCompositionalLayout{section, _ in
             switch Section(rawValue: section) {
             case .banner:
-                return HomeBannerCollectionViewCell.bannerLayout()
+                return HomeBannerCollectionViewCell.bannerLayout()// 배너 섹션의 레이아웃을 반환하는것
             case .horizontalProductItem:
                 return  HomeProductCollectionViewCell.horizontalProductItemLayout()
                 

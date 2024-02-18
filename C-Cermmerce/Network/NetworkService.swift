42a6d74f7bde79781e9048053435909c6b7bf146
sikwan <yysskk99999@naver.com>
2024-02-18 21:04:01 +0900

    network service code refactoring

--- C-Cermmerce/Network/NetworkService.swift
+++ C-Cermmerce/Network/NetworkService.swift
@@ -10,12 +10,24 @@ import Foundation
 class NetworkService {
     static let shared : NetworkService = NetworkService()
     
-    func getHoemData() async throws -> HomeResponse{
-        let urlString = "https://my-json-server.typicode.com/JeaSungLEE/JsonAPIFastCampus/db"
+    private let hostURL = "https://my-json-server.typicode.com/JeaSungLEE/JsonAPIFastCampus"
+    
+    private func createURL(withPath path: String) throws -> URL {
+        let urlString = "\(hostURL)\(path)"
         guard let url = URL(string : urlString) else {throw URLError(.badURL)}
-        
+        return url
+    }
+    
+    private func fetchData(from url : URL) async throws -> Data {
         let (data,response) = try await URLSession.shared.data(from: url)
         guard let httpResponse = response as? HTTPURLResponse,httpResponse.statusCode == 200 else {throw URLError(.badServerResponse)}
+        return data
+    }
+    
+    func getHoemData() async throws -> HomeResponse{
+        let url = try createURL(withPath: "/db")
+        let data = try await fetchData(from: url)
+       
         let decodeData = try JSONDecoder().decode(HomeResponse.self,from: data)
         return decodeData
     }

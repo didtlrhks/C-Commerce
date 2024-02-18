72bd4614dac1c5f46b1cd92ba2aed373d576d9db
sikwan <yysskk99999@naver.com>
2024-02-18 21:10:28 +0900

    networkerror code refactoring

--- C-Cermmerce/Network/NetworkService.swift
+++ C-Cermmerce/Network/NetworkService.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+enum NetworkError : Error {
+    case responseError
+    case decodeError
+    case urlError
+    case serverError(statusCode : Int)
+    case unknownError
+}
+
 class NetworkService {
     static let shared : NetworkService = NetworkService()
     
@@ -14,22 +22,35 @@ class NetworkService {
     
     private func createURL(withPath path: String) throws -> URL {
         let urlString = "\(hostURL)\(path)"
-        guard let url = URL(string : urlString) else {throw URLError(.badURL)}
+        guard let url = URL(string : urlString) else {throw NetworkError.urlError}
         return url
     }
     
     private func fetchData(from url : URL) async throws -> Data {
         let (data,response) = try await URLSession.shared.data(from: url)
-        guard let httpResponse = response as? HTTPURLResponse,httpResponse.statusCode == 200 else {throw URLError(.badServerResponse)}
-        return data
+        guard let httpResponse = response as? HTTPURLResponse else {throw NetworkError.responseError}
+        
+        switch httpResponse.statusCode {
+        case 200...299:
+            return data
+        default:
+            throw NetworkError.serverError(statusCode: httpResponse.statusCode)
+        }
+       // return data
     }
     
     func getHoemData() async throws -> HomeResponse{
         let url = try createURL(withPath: "/db")
         let data = try await fetchData(from: url)
+        do {
+            let decodeData = try JSONDecoder().decode(HomeResponse.self,from: data)
+            return decodeData
+        } catch {
+           throw NetworkError.decodeError
+        }
        
-        let decodeData = try JSONDecoder().decode(HomeResponse.self,from: data)
-        return decodeData
+      
+    
     }
     
 }

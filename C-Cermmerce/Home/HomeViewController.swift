fcc8f5140b20473e01ed2ad6da3e4ba4ca43c118
sikwan <yysskk99999@naver.com>
2024-02-19 19:26:40 +0900

    couponButtonLayout add

--- C-Cermmerce/Home/HomeViewController.swift
+++ C-Cermmerce/Home/HomeViewController.swift
@@ -13,6 +13,7 @@ final class HomeViewController:UIViewController {
     enum Section : Int{ //섹션의 종류를 정의하는 열거형
         case banner
         case horizontalProductItem
+        case couponButton
         case verticalProductItem
         
     }
@@ -21,24 +22,24 @@ final class HomeViewController:UIViewController {
     @IBOutlet private weak var collectionView: UICollectionView! // 뷰와의 연결
     private var dataSource : UICollectionViewDiffableDataSource<Section, AnyHashable>? // 컬렉션 뷰의 데이터 소스를 관리하는 변수임
     private var compositianlLayout : UICollectionViewCompositionalLayout = setCompositinalLayout()
-        
-        //UICollectionViewCompositionalLayout를 사용하여 각 섹션의 레이아웃을 동적으로 생성
+    
+    //UICollectionViewCompositionalLayout를 사용하여 각 섹션의 레이아웃을 동적으로 생성
     private var viewModel : HomeViewModel = HomeViewModel() // 홈화면의 데이터 처리와 상태관리를 담당하기위해서 사용함
     private var cancellables : Set<AnyCancellable> = [] // 콤바인을 사용해서 뷰모델의 상태변화를 구독하는 cancellables 을 만들어줌
-
+    
     
     
     override func viewDidLoad() { // 뷰 컨트롤러의 뷰가 로드될때 호출되는녀석
         super.viewDidLoad()
-      
+        
         bindingViewModel()//뷰모델 바인딩
-       
+        
         setDataSource()// 데이터 소스설정하는 함수사용
         
-      
+        
         
         collectionView.collectionViewLayout = compositianlLayout//컬렉션 뷰의 레이아웃설정
-
+        
         viewModel.process(action: .loadData)//데이터 로드작업
     }
     
@@ -49,6 +50,8 @@ final class HomeViewController:UIViewController {
                 return HomeBannerCollectionViewCell.bannerLayout()// 배너 섹션의 레이아웃을 반환하는것
             case .horizontalProductItem:
                 return  HomeProductCollectionViewCell.horizontalProductItemLayout()
+            case .couponButton:
+                return HomeCouponButtonCollectionViewCell.couponButtonItemLayout()
                 
             case .verticalProductItem:
                 return  HomeProductCollectionViewCell.verticalProductItemLayout()
@@ -64,23 +67,24 @@ final class HomeViewController:UIViewController {
                 
             }.store(in: &cancellables)
     }
-   
+    
     
     private func setDataSource() {
         dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView,cellProvider: { [weak self] collectionView, indexPath, viewModel in
-
+            
             switch Section(rawValue: indexPath.section) {
             case .banner :
                 return self?.bannerCell(collectionView,indexPath,viewModel)
                 
             case .horizontalProductItem, .verticalProductItem:
                 return self?.ProductItemCell(collectionView,indexPath,viewModel)
-               
+            case .couponButton:
+                return self?.couponButtonCell(collectionView,indexPath,viewModel)
             case .none :
                 return .init()
             }
             
-           
+            
         })
     }
     
@@ -106,22 +110,29 @@ final class HomeViewController:UIViewController {
     private func bannerCell(_ collectionView: UICollectionView,_ indexPath: IndexPath,_ viewModel:AnyHashable) -> UICollectionViewCell{
         guard let viewModel = viewModel as? HomeBannerCollectionViewCellViewModel,
               let cell : HomeBannerCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeBannerCollectionViewCell", for: indexPath) as? HomeBannerCollectionViewCell else {return .init() }
-       
+        
         cell.setViewModel(viewModel)
         return cell
     }
     private func ProductItemCell(_ collectionView: UICollectionView,_ indexPath: IndexPath,_ viewModel:AnyHashable) -> UICollectionViewCell{
         guard let viewModel = viewModel as? HomeProductCollectionViewCellViewModel,
-
-                 let cell : HomeProductCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeProductCollectionViewCell", for : indexPath) as?
-                 HomeProductCollectionViewCell else {return .init()}
-         cell.setViewModel(viewModel)
-         return cell
+              
+                let cell : HomeProductCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeProductCollectionViewCell", for : indexPath) as?
+                HomeProductCollectionViewCell else {return .init()}
+        cell.setViewModel(viewModel)
+        return cell
     }
-
-
+    
+    private func couponButtonCell(_ collectionView: UICollectionView,_ indexPath: IndexPath,_ viewModel:AnyHashable) -> UICollectionViewCell{
+        
+        guard let viewModel = viewModel as? HomeCouponButtonCollectionViewCellViewModel,
+              
+                let cell : HomeCouponButtonCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCouponButtonCollectionViewCell", for : indexPath) as?
+                HomeCouponButtonCollectionViewCell else {return .init()}
+        cell.setViewModel(viewModel)
+        return cell
+    }   
 }
-
 #Preview {
     UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController() as!
     HomeViewController
--- C-Cermmerce/Home/퍋ㅈ/HomeCouponButtonCollectionViewCell.swift
+++ C-Cermmerce/Home/퍋ㅈ/HomeCouponButtonCollectionViewCell.swift
@@ -38,3 +38,21 @@ final class HomeCouponButtonCollectionViewCell: UICollectionViewCell {
         }
     }
 }
+
+
+extension HomeCouponButtonCollectionViewCell {
+    static func couponButtonItemLayout() -> NSCollectionLayoutSection {
+        let itemSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item : NSCollectionLayoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize : NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(37))
+        
+        let group : NSCollectionLayoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section : NSCollectionLayoutSection = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
+        section.contentInsets = .init(top:28,leading: 22,bottom: 0,trailing: 22)
+       
+        return section
+    }
+}
